4071683b5ce25d4405815ddd040325495cd0c627
mfrei <matzefrei@gmail.com>
2020-05-11 17:36:58 +0200

    feat: Build submit trivia layout
    - Create new fragment
    - Build layout for to submit new trivia
    - Add to drawer navigation

--- app/src/main/java/com/madfree/capstoneproject/MainActivity.java
+++ app/src/main/java/com/madfree/capstoneproject/MainActivity.java
@@ -117,6 +117,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 getSupportFragmentManager().beginTransaction()
                         .replace(R.id.fragment_container, new HomeFragment()).commit();
                 break;
+            case R.id.nav_submit:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new SubmitFragment()).commit();
+                break;
         }
         drawer.closeDrawer(GravityCompat.START);
         return true;
