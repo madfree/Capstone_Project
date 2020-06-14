41db63f5e9d525b60cb3d75518b19a70553ee01b
mfrei <matzefrei@gmail.com>
2020-06-14 20:37:22 +0200

    feat: Add Firebase persistence to app
    - Add code for persistence to application level
    - Move text from layout into strings.xml

--- app/src/main/java/com/madfree/capstoneproject/util/MyApplication.java
+++ app/src/main/java/com/madfree/capstoneproject/util/MyApplication.java
@@ -1,15 +1,15 @@
 package com.madfree.capstoneproject.util;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 import timber.log.Timber;
 
 public class MyApplication extends android.app.Application {
-
 
     @Override
     public void onCreate() {
         super.onCreate();
         Timber.plant(new Timber.DebugTree());
-        //FirebaseDatabase.getInstance().setPersistenceEnabled(true);
-
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
     }
 }
